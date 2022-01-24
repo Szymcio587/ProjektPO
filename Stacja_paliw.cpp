302b8226d3d74f29c0d881f55ab3285a9f043244
Szymon Wójcik <408331@student.agh.edu.pl>
2022-01-24 03:03:22 +0100

    Revert "dostawa"
    This reverts commit 2e78fbc7c3cf95e40cab00fa150feeea03c87d32.

--- Stacja_paliw.cpp
+++ Stacja_paliw.cpp
@@ -20,14 +20,14 @@ int main() {
 	char odpowiedz;
 	Dystrybutor D1;
 	Myjnia M1;
-	Klient K1(40, "Tlokowy", "Jan-Krzysztof Duda", true);
+	Klient K1(29, "Tlokowy", "Jan-Krzysztof Duda", true);
 	Pracownik P1("Gosia", "Samosia", 7);
 	Sklep S1;
 	Manager Man;
 	Dostawa Dos1("cos", true, P1);
 
 	for (;;) {
-		cout << "\n\n1.Tankowanie\n2.Myjnia\n3.Sklep\n4.Dostawa";
+		cout << "\n\n1.Tankowanie\n2.Myjnia\n3.Sklep\n4.Dostawa\n5.Tryb menadzera";
 		cout << endl << "Witaj! Z czego chcesz skorzystac?" << endl;
 		cin >> wybor;
 		switch (wybor)
@@ -35,7 +35,9 @@ int main() {
 		case 1: { D1.uruchom_funkcje(&K1);  cout << "Stan konta klienta " << K1.getStanKonta() << endl; } break;
 		case 2: { M1.uruchom_funkcje(&K1);  cout << "Stan konta klienta " << K1.getStanKonta() << endl; } break;
 		case 3: { P1.obsluz_sklep(S1, &K1);  cout << "Stan konta klienta " << K1.getStanKonta() << endl; } break;
-		case 4: { Dos1.zacznij_dostawe(S1); } break;
+		case 4:  break;
+		case 5:	 break;
+		case 6: break;
 		}
 		cout << "Czy chcesz skorzystac z uslug jeszcze raz? (t/n)";
 		cin >> odpowiedz;
